9ec20eeeb56ced9f3fe2803f21baf8e393810856
havefunli <1641869338@qq.com>
2024-11-02 21:49:47 +0800

    完善了Any的功能

--- Any.hpp
+++ Any.hpp
@@ -1,55 +1,94 @@
 #pragma once
 #include <typeinfo>
 #include <memory>
+#include <utility>
+#include <assert.h>
 
 class Any
 {
-public:
-	Any(){}
-	
-	template <class T>
-	Any(const T& val)
-		: _ptr(std::make_unique<holder<T>>(val))
-	{}
-
-	Any(const Any& other)
-		: _ptr(other._ptr ? other._ptr->clone() : nullptr)
-	{}
-
-	// Any& 
-
 private:
-	class placeholder
+	class holder
 	{
 	public:
+		holder()  = default;
+		~holder() = default;
+
 		virtual const std::type_info& type() = 0;
-		virtual placeholder* clone() = 0;
+		virtual holder* clone() = 0;
 	};
 
-	template <class T>
-	class holder : public placeholder
+	template <class ValueType>
+	class placeholder : public holder
 	{
 	public:
-		holder(const T& val)
+		placeholder()
+			: _val(ValueType())
+		{}
+
+		placeholder(const ValueType& val)
 			: _val(val)
 		{}
 
 		const std::type_info& type() override
 		{
-			return typeid(T);
-		}
-		
-		placeholder* clone() override
-		{
-			return new holder(_val);
+			return typeid(ValueType);
 		}
 
-	private:
-		T _val;
+		holder* clone() override
+		{
+			return new placeholder(_val);
+		}
+
+		ValueType _val;
 	};
 
+public:
+	Any()
+		: _ptr(nullptr)
+	{}
+
+	template <class T>
+	Any(const T& val)
+		: _ptr(std::make_shared<placeholder<T>>(val))
+	{}
+
+	template <class T>
+	Any(const Any& other)
+	{
+		_ptr = (other._ptr.get() ? other._ptr->clone() : nullptr);
+	}
+
+	void Swap(Any other)
+	{
+		std::swap(other._ptr, _ptr);
+	}
+
+	Any& operator=(const Any& other)
+	{
+		if (this != &other)
+			Swap(other);
+		return *this;
+	}	
+
+	template <class T>
+	Any& operator=(const T& val)
+	{
+		_ptr = std::make_shared<placeholder<T>>(val);
+		return *this;
+	}
+
+	template <class T>
+	T& Get()
+	{
+		assert(typeid(T) == _ptr->type());
+
+		if (_ptr.get() == nullptr) throw std::bad_cast();;
+		placeholder<T>* holder = static_cast<placeholder<T>*>(_ptr.get());
+		return holder->_val;
+	}
+
 private:
-	std::unique_ptr<placeholder> _ptr;
+	std::shared_ptr<holder> _ptr;
 };
 
 
