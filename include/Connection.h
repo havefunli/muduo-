43ee859eecf2385d92d605546767451f0393a1a0
havefunli <1641869338@qq.com>
2024-11-16 22:51:39 +0800

    完善了 Connection 的功能，但是存在接受新连接断开的 Bug

--- include/Connection.h
+++ include/Connection.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "Channel.h"
 #include "EventLoop.h"
 #include "../src/Socket.hpp"
@@ -19,9 +18,9 @@ using ConnPtr = std::shared_ptr<Connection>;
 const int DefaultTimeout = 60;
 
 using ConnectedCallBack = std::function<void(ConnPtr)>;
-using MessageCallBack   = std::function<void(ConnPtr)>;
 using ClosedCallBack    = std::function<void(ConnPtr)>;
 using AnyEventCallBack  = std::function<void(ConnPtr)>;
+using MessageCallBack   = std::function<void(ConnPtr, BufferPtr)>;
 
 class Connection : public std::enable_shared_from_this<Connection>
 {
@@ -43,34 +42,54 @@ private:
     // - 用户定义的任务
     void HandleEvent();
 
-    // 在关闭连接前处理剩余数据
     void ShutDownInLoop();
 
-    // 连接监控后，进行的设置，启动读监控，调用回调
     void EstablishedLoop();
 
     // 真正地关闭操作
     void ReleaseInLoop();
 
-    void SendInLoop(char* data, size_t n);
+    void SendInLoop(const char*, size_t);
 
-    // 管理连接定时器任务
     void EnableTimerInLoop(bool, int sec);
 
+    // 更新回调
+    void UpgradeInLoop(ConnectedCallBack,
+                       MessageCallBack,
+                       ClosedCallBack,
+                       AnyEventCallBack);
+
 public:
-    Connection();
+    Connection(int conn_id, int sockfd, EveLoopPtr loop);
     
     ~Connection();
     
+    int GetFd();
+
+    uint16_t GetId();
+
+    ConStatus GetStatus();
+
     // 发送数据，将数据写入发送缓冲区，打开写事件监控
-    void Send(char* data, size_t n);
+    void Send(const char*, size_t);
 
     // 关闭连接，但是首先需要判断数据是否处理完毕
-    void CloseConn();
+    void ShutDown();
 
+    // 管理连接定时器任务
     // 是否启用活跃监控，并且定时时间(默认 1 min)
     void EnableMonitorActivity(bool, int sec = DefaultTimeout);
-     
+    
+    // 连接监控后，进行的设置，启动读监控，调用回调
+    void Established();
+    
+    // 下面的接口为用户提供
+    // 用户自定义在发生特定事件后的回调函数
+    void SetConnectedCallBack(const ConnectedCallBack&); // 连接成功回调
+    void SetMessageCallBack(const MessageCallBack&);     // 接受数据后的回调
+    void SetClosedCallBack(const ClosedCallBack&);       // 连接关闭后的回调
+    void SetAnyEventCallBack(const AnyEventCallBack&);   // 任何事件的回调
+    void SetSrvClosedCallBack(const ClosedCallBack&);    // 服务器的关闭回调
 
 private:
     int        _sockfd;    // 管理的套接字
@@ -79,14 +98,14 @@ private:
     SockPtr    _socket;    // 套接字的事件管理
     ChannelPtr _channel;   // 连接的事件管理
     EveLoopPtr _loop;      // 关联的 eventloop
-    Buffer     _inbuf;     // 输入缓冲区
-    Buffer     _outbuf;    // 输出缓冲区
+    BufferPtr  _inbuf;     // 输入缓冲区
+    BufferPtr  _outbuf;    // 输出缓冲区
     Any        _content;   // 请求的接受处理上下文
     bool       _istmract;  // 是否激活计时器
 
     ConnectedCallBack _conn_cb;
     MessageCallBack   _msg_cb;
     ClosedCallBack    _closed_cb;
-    ClosedCallBack    _server_closed_cb;  // 服务器的关闭回调
     AnyEventCallBack  _any_cb;
+    ClosedCallBack    _svr_closed_cb;  
 };
--- src/Connection.cpp
+++ src/Connection.cpp
@@ -1,5 +1,38 @@
 #include "../include/Connection.h"
 
+Connection::Connection(int conn_id, int sockfd, EveLoopPtr loop)
+    : _conn_id(conn_id)
+    , _sockfd(sockfd)
+    , _istmract(false)
+    , _status(CONNECTING)
+    , _loop(loop)
+    , _inbuf(std::make_shared<Buffer>())
+    , _outbuf(std::make_shared<Buffer>())
+{
+    _channel->SetEventLoop(loop.get());
+    _channel->RigisterEventsFunc(std::bind(&Connection::HandleRead, this),
+                                 std::bind(&Connection::HandleWrite, this),
+                                 std::bind(&Connection::HandleError, this));
+    _channel->SetCloseFunc(std::bind(&Connection::HandleClose, this));
+    _channel->SetEventCallBack(std::bind(&Connection::HandleEvent, this));
+}
+
+Connection::~Connection() = default;
+
+int Connection::GetFd()
+{
+    return _sockfd;
+}
+
+uint16_t Connection::GetId()
+{
+    return _conn_id;
+} 
+
+ConStatus Connection::GetStatus()
+{
+    return _status;
+}
 
 void Connection::HandleRead()
 {
@@ -8,33 +41,33 @@ void Connection::HandleRead()
     int n = _socket->Recv(buf);
     if (n < 0) { return ShutDownInLoop(); } // 读取出错
     // 在这里等于 0 不再是断开连接
-    _inbuf.Write(buf.c_str(), buf.size()); // 写入缓冲区
+    _inbuf->Write(buf.c_str(), buf.size()); // 写入缓冲区
     
     // 数据处理
-    if (_inbuf.ReadableBytes())
+    if (_inbuf->ReadableBytes())
     {
-        _msg_cb(shared_from_this());
+        _msg_cb(shared_from_this(), _inbuf);
     }
 }
  
 void Connection::HandleWrite()
 {
-    int n = _socket->Send(_outbuf.ReadAllContent());
+    int n = _socket->Send(_outbuf->ReadAllContent());
     // 发送事件异常
     if (n < 0) 
     {
         // 有无数据没处理, 有则处理
-        if (_inbuf.ReadableBytes()) { _msg_cb(shared_from_this()); }
+        if (_inbuf->ReadableBytes()) { _msg_cb(shared_from_this(), _inbuf); }
         ReleaseInLoop(); // 关闭操作
     }
     // 若没有数据处理了，关闭写事件
-    if (_outbuf.ReadableBytes() == 0) { _channel->EnableWriteable(false); }
+    if (_outbuf->ReadableBytes() == 0) { _channel->EnableWriteable(false); }
 }
 
 void Connection::HandleClose()
 {
     // 查看是否剩余数据处理
-    if (_inbuf.ReadableBytes()) { _msg_cb(shared_from_this()); }
+    if (_inbuf->ReadableBytes()) { _msg_cb(shared_from_this(), _inbuf); }
     ReleaseInLoop(); // 关闭操作
 }
 
@@ -49,15 +82,6 @@ void Connection::HandleEvent()
     if (_any_cb) { _any_cb(shared_from_this()); }
 }
 
-void Connection::EstablishedLoop()
-{
-    // 设置为连接状态
-    _status = CONNECTED;
-    _channel->EnableReadable(true);
-    // 连接事件回调
-    if (_conn_cb) { _conn_cb(shared_from_this()); }
-}
-
 void Connection::ReleaseInLoop()
 {
     _status = DISCONNECTED;
@@ -70,29 +94,53 @@ void Connection::ReleaseInLoop()
     // 调用用户关闭回调
     if (_closed_cb) { _closed_cb(shared_from_this()); }
     // 调用服务器的关闭回调
-    if (_server_closed_cb) { _server_closed_cb(shared_from_this()); }
+    if (_svr_closed_cb) { _svr_closed_cb(shared_from_this()); }
 }
 
-void Connection::SendInLoop(char* data, size_t n)
+void Connection::EstablishedLoop()
+{
+    // 设置为连接状态
+    _status = CONNECTED;
+    _channel->EnableReadable(true);
+    // 连接事件回调
+    if (_conn_cb) { _conn_cb(shared_from_this()); }
+}
+
+void Connection::Established()
+{
+    _loop->RunInLoop(std::bind(&Connection::EstablishedLoop, this));
+}
+
+void Connection::SendInLoop(const char* data, size_t n)
 {
     // 将数据写入缓冲区
-    _outbuf.Read(data, n);
+    _outbuf->Write(data, n);
     // 开启写监控，帮我们发送
     _channel->EnableWriteable(true);
 }
 
+void Connection::Send(const char* data, size_t n)
+{
+    _loop->RunInLoop(std::bind(&Connection::SendInLoop, this, data, n));
+}
+
 void Connection::ShutDownInLoop()
 {
-    _status = CONNECTING;
+    _status = DISCONNECTING;
     // 还有数据未处理
-    if (_inbuf.ReadableBytes())
+    if (_inbuf->ReadableBytes())
     {
-        if (_msg_cb) { _msg_cb(shared_from_this()); }
+        if (_msg_cb) { _msg_cb(shared_from_this(), _inbuf); }
     }
     // 查看还是否存在数据未发送
-    if (_outbuf.ReadableBytes()) { _channel->EnableWriteable(true); }
+    if (_outbuf->ReadableBytes()) { _channel->EnableWriteable(true); }
     // 真正的关闭
     ReleaseInLoop();
+}
+
+void Connection::ShutDown()
+{
+    _loop->RunInLoop(std::bind(&Connection::ShutDownInLoop, this));
 }
 
 void Connection::EnableTimerInLoop(bool isvalid, int timeout)
@@ -102,11 +150,53 @@ void Connection::EnableTimerInLoop(bool isvalid, int timeout)
     {
         _istmract = true;
         if (_loop->HasTimer(_conn_id)) { return _loop->RefreshTimer(_conn_id); }
-        else { _loop->AddTimer(_conn_id, timeout, std::bind(Connection::ReleaseInLoop, this)); }
+        else { _loop->AddTimer(_conn_id, timeout, std::bind(&Connection::ReleaseInLoop, this)); }
     }
     else
     {
         _istmract = false;
         if (_loop->HasTimer(_conn_id)) { _loop->EnableTimer(_conn_id, false); }
     }
+}
+
+void Connection::EnableMonitorActivity(bool isvalid, int timeout)
+{
+    _loop->RunInLoop(std::bind(&Connection::EnableTimerInLoop, this, isvalid, timeout));
+}
+
+// 更新回调
+void Connection::UpgradeInLoop(ConnectedCallBack con_cb,
+                               MessageCallBack msg_cb,
+                               ClosedCallBack  close_cb,
+                               AnyEventCallBack event_cb)
+{
+    _conn_cb   = _conn_cb;
+    _msg_cb    = msg_cb;
+    _closed_cb = _closed_cb;
+    _any_cb    = event_cb;
+}
+
+void Connection::SetConnectedCallBack(const ConnectedCallBack& call_back) 
+{
+    _conn_cb = call_back;
+}
+
+void Connection::SetMessageCallBack(const MessageCallBack& call_back)
+{
+    _msg_cb = call_back;
+}
+
+void Connection::SetClosedCallBack(const ClosedCallBack& call_back)
+{
+    _closed_cb = call_back;
+}
+
+void Connection::SetAnyEventCallBack(const AnyEventCallBack& call_back)
+{
+    _any_cb = call_back;
+}
+
+void Connection::SetSrvClosedCallBack(const ClosedCallBack& call_back)
+{
+    _svr_closed_cb = call_back;
 }
